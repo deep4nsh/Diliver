c1a4048cb9d2a0063c8c48030d3342ef4a757eea
Deepansh Gupta <deepansh.gup124@gmail.com>
2025-03-25 12:02:01 +0530

    Diliver

--- app/src/main/java/deedev/diliver/adapter/BuyAgainAdapter.kt
+++ app/src/main/java/deedev/diliver/adapter/BuyAgainAdapter.kt
@@ -1,10 +1,39 @@
 package deedev.diliver.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import deedev.diliver.databinding.BuyAgainItemBinding
 
 class BuyAgainAdapter(private val buyAgainFoodNames: ArrayList<String>,
     private val buyAgainFoodPrice: ArrayList<String>,
     private val buyAgainFoodImage: ArrayList<Int>): RecyclerView.Adapter<BuyAgainAdapter.BuyAgainViewHolder>() {
-    annotation class BuyAgainViewHolder
 
+    override fun onBindViewHolder(holder: BuyAgainViewHolder, position: Int) {
+        holder.bind(buyAgainFoodNames[position],buyAgainFoodPrice[position],buyAgainFoodImage[position])
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuyAgainViewHolder {
+        val binding=BuyAgainItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return BuyAgainViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int=buyAgainFoodNames.size
+    class BuyAgainViewHolder(private val binding: BuyAgainItemBinding): RecyclerView.ViewHolder
+        (binding.root) {
+        fun bind(
+            foodName: String,
+            foodPrice: String,
+            foodImage: Int
+        ) {
+            binding.buyAgainFoodName.text=foodName
+            binding.buyAgainFoodPrice.text=foodPrice
+            binding.buyAgainFoodImage.setImageResource(foodImage)
+            binding.buyAgainFoodButton.setOnClickListener {
+
+            }
+        }
+    }
 }
+
+
--- app/src/main/java/deedev/diliver/fragment/HistoryFragment.kt
+++ app/src/main/java/deedev/diliver/fragment/HistoryFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import deedev.diliver.R
+import deedev.diliver.adapter.BuyAgainAdapter
+import deedev.diliver.databinding.FragmentHistoryBinding
 
 
 class HistoryFragment : Fragment() {
-
+    private lateinit var binding: FragmentHistoryBinding
+    private lateinit var buyAgainAdapter: BuyAgainAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -20,10 +24,18 @@ class HistoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding= FragmentHistoryBinding.inflate(layoutInflater,container,false)
+        setupRecyclerView()
+        return (binding.root)
     }
-
+    private fun setupRecyclerView() {
+        val buyAgainFoodName = arrayListOf("Food 1","Food 2", "food 3")
+        val buyAgainFoodPrice = arrayListOf("100 Rs.", "150 Rs.", "99 Rs.")
+        val buyAgainFoodImage = arrayListOf(R.drawable.pizza_image, R.drawable.momo, R.drawable.burger)
+        val buyAgainAdapter= BuyAgainAdapter(buyAgainFoodName,buyAgainFoodPrice,buyAgainFoodImage)
+        binding.BuyAgainRecyclerView.adapter=buyAgainAdapter
+        binding.BuyAgainRecyclerView.layoutManager= LinearLayoutManager(requireContext())
+    }
 
 
 }
